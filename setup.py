0752c55a0ce5cf807a97d0dcb60943b423fd4952
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2013-04-11 20:33:52 +0200

    binary name changed to something fancier

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+from distutils.core import setup
+
+version = '0.1'
+
+setup(
+  name='chinese-checkers',
+  version=version,
+  description='Python implementation of the chinese checkers game, GUI and AI',
+  packages=['chinese-checkers'],
+  scripts=['chinese-checkers/chinese-checkers-gui'],
+  license = "GPL3",
+  url = "https://github.com/ltworf/tin171",
+)
